c1204e8d9099fba118e1e7af3c7eb5ae1019375e
Ubugeeei <ubuge1122@gamil.com>
2022-03-27 23:38:03 +0900

    refactor: impl main function on constractor function pattern

--- src/constructor_pattern/constructor_function/example.js
+++ src/constructor_pattern/constructor_function/example.js
@@ -2,31 +2,41 @@ function _constructor(c) {
 	Object.assign(this, c);
 }
 
-const instance = new _constructor({
-	name: "John",
-	age: 30,
-	married: true,
-	address: {
-		city: "New York",
-		street: "Wall Street",
-	},
-});
-console.log("🚀 ~ file: example.js ~ line 6 ~ instance\n", instance, "\n");
-
-// strictly
 function Person(arg) {
 	this.name = arg.name;
 	this.age = arg.age;
 	this.married = arg.married;
 	this.address = arg.address;
 }
-const person = new Person({
-	name: "John",
-	age: 30,
-	married: true,
-	address: {
-		city: "New York",
-		street: "Wall Street",
-	},
-})
-console.log("🚀 ~ file: example.js ~ line 32 ~ person\n", person, "\n")
+
+(function main() {
+	const instance = new _constructor({
+		name: "John",
+		age: 30,
+		married: true,
+		address: {
+			city: "New York",
+			street: "Wall Street",
+		},
+	});
+	console.log(
+		"🚀 ~ file: example.js ~ line 23 ~ instance\n",
+		instance,
+		"\n"
+	);
+
+	const person = new Person({
+		name: "John",
+		age: 30,
+		married: true,
+		address: {
+			city: "New York",
+			street: "Wall Street",
+		},
+	});
+	console.log(
+		"🚀 ~ file: example.js ~ line 38 ~ person\n",
+		person,
+		"\n"
+	);
+})();
