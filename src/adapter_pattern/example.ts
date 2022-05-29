d92fa2b86f8ddf3e0305f90c68292c88ad78826e
Ubugeeei <ubuge1122@gamil.com>
2022-05-29 18:42:45 +0900

    feat: impl adapter pattern

--- src/adapter_pattern/example.ts
+++ src/adapter_pattern/example.ts
@@ -14,10 +14,9 @@ class Banner {
 }
 
 interface Print {
- printWeak(): void
+  printWeak(): void
   printStrong(): void
 }
-
 
 class PrintBanner extends Banner implements Print {
   printWeak(): void {
@@ -37,3 +36,53 @@ class PrintBanner extends Banner implements Print {
   print.printWeak()
   print.printStrong()
 })()
+
+/**
+ * -----------------
+ * use adapter
+ * -----------------
+ */
+class ABanner {
+  private readonly str: string
+  constructor(str: string) {
+    this.str = str
+  }
+
+  showWithParen(): void {
+    console.log(`(${this.str})`)
+  }
+
+  showWithAster(): void {
+    console.log(`*${this.str}*`)
+  }
+}
+
+abstract class APrint {
+  abstract printWeak(): void
+  abstract printStrong(): void
+}
+
+class APrintBanner extends APrint {
+  private readonly banner: ABanner
+  constructor(str: string) {
+    super()
+    this.banner = new ABanner(str)
+  }
+
+  printWeak(): void {
+    this.banner.showWithParen()
+  }
+
+  printStrong(): void {
+    this.banner.showWithAster()
+  }
+}
+
+/**
+ * exec
+ */
+ ;(function main() {
+  const print: Print = new APrintBanner("Hello World")
+  print.printWeak()
+  print.printStrong()
+ })()
